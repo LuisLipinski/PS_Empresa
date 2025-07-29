dc2f2b40b22dd730a6b5e149682d7aca7bf9437f
Luis Henrique Lipinski <lhlipinski@gmail.com>
2025-07-28 22:57:26 -0300

    configurado o banco h2 para o CI

--- src/test/java/com/mypetadmin/ps_empresa/PsEmpresaApplicationTests.java
+++ src/test/java/com/mypetadmin/ps_empresa/PsEmpresaApplicationTests.java
@@ -2,8 +2,10 @@ package com.mypetadmin.ps_empresa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PsEmpresaApplicationTests {
 
 	@Test
