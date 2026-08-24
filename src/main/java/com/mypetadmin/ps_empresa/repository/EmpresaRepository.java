009dfda134d5d812938f4381afbbc4cf63b2aa63
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-24 14:42:16 -0300

    fix: ignore current empresa when checking email uniqueness

--- src/main/java/com/mypetadmin/ps_empresa/repository/EmpresaRepository.java
+++ src/main/java/com/mypetadmin/ps_empresa/repository/EmpresaRepository.java
@@ -1,15 +1,16 @@
 package com.mypetadmin.ps_empresa.repository;
 
 import com.mypetadmin.ps_empresa.model.Empresa;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, UUID>, JpaSpecificationExecutor<Empresa> {
+
     boolean existsByDocumentNumber(String documentNumber);
 
     boolean existsByEmail(String email);
+
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
